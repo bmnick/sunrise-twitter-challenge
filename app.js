c13f3e7d031fd51f3dbe52a9fec92166fe72626e
Ben Nicholas <ben@ben-nicholas.com>
2013-05-03 01:30:45 -0700

    Add support for low-volume searches
    Low-volume searches are those who do not always return new values as
    fast as we can search. So when we don't have a result, we just pass
    through without touching anything, to approach streaming

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 
 var TwitterSearch = require('./src/twitterSearch.js').TwitterSearch;
 
-var search_string = process.argv[2];
+var query = process.argv[2];
 var last_id = 0;
 
 var START_COLOR = "\033[94m";
@@ -12,17 +12,27 @@ function printTweet(tweet) {
 	console.log(START_COLOR + tweet['from_user'] + ": " + END_COLOR + tweet['text']);
 }
 
+function processTweets(tweets) {
+	// First, store away our newest id so we know where to continue from
+	last_id = tweets[0]['id'];
+
+	// Since we're printing in order, let's go ahead and reorder them
+	tweets.reverse;
+	for (var index in tweets) {
+		var tweet = tweets[index];
+
+		printTweet(tweet);
+	}
+}
+
+function checkTweets(tweets) {
+	if (tweets.length > 0) {
+		processTweets(tweets);
+	}
+}
+
 function fetchTweets() {
-	var tweets = TwitterSearch.search(search_string, function(tweets) {
-		last_id = tweets[0]['id']; // newest are placed first
-
-		tweets.reverse
-		for (var index in tweets) {
-			var tweet = tweets[index];
-
-			printTweet(tweet);
-		}
-	}, last_id);
+	var tweets = TwitterSearch.search(query, checkTweets, last_id);
 }
 
 fetchTweets();

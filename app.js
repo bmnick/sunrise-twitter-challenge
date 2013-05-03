0210ab0ffc14a45f769c4f17d51a5921765c82a4
Ben Nicholas <ben@ben-nicholas.com>
2013-05-03 01:35:13 -0700

    Added a bit more documentation

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 #!/usr/local/bin/node
 
+// ==== Imports
 var TwitterSearch = require('./src/twitterSearch.js').TwitterSearch;
 
-var query = process.argv[2];
-var last_id = 0;
-
+// ==== Constants
 var START_COLOR = "\033[94m";
 var END_COLOR = "\033[0m";
 
+// ==== State
+var query = process.argv[2];
+var last_id = 0;
+
+// ==== Functions
 function printTweet(tweet) {
 	console.log(START_COLOR + tweet['from_user'] + ": " + END_COLOR + tweet['text']);
 }
@@ -18,6 +22,8 @@ function processTweets(tweets) {
 
 	// Since we're printing in order, let's go ahead and reorder them
 	tweets.reverse;
+
+	// Loop over our newly ordered tweets and print them
 	for (var index in tweets) {
 		var tweet = tweets[index];
 
@@ -35,6 +41,7 @@ function fetchTweets() {
 	var tweets = TwitterSearch.search(query, checkTweets, last_id);
 }
 
+// ==== Main
 fetchTweets();
 setInterval(fetchTweets, 5000 );
 
